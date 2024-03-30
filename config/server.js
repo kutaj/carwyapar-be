9a4b9422c6301d774e834391677e41476358d5d7
Pavan <pavan@techflair.in>
2024-03-30 19:54:21 +0530

    Server changes

--- config/server.js
+++ config/server.js
@@ -1,10 +1,11 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('BACKEND_URL', 'https://api.carwyapar.com'),
   app: {
     keys: env.array('APP_KEYS'),
+    proxy: true
   },
-  url: env('BACKEND_URL'),
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
