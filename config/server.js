8901951d8512617ab41ec2c920ff52b5c142aea8
RajaSharmaIndia <mail2raja.india@gmail.com>
2024-05-26 11:40:46 +0530

    Server Js Folder Update

--- config/server.js
+++ config/server.js
@@ -1,13 +1,19 @@
 module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  url: env('BACKEND_URL'),
   proxy: true,
   app: {
-    keys: env.array("APP_KEYS"),
+    keys: env.array('APP_KEYS'),
     proxy: true,
   },
-  url: env("BACKEND_URL"),
   webhooks: {
-    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
+  admin: {
+    url: env('PUBLIC_ADMIN_URL', '/'),
+    auth: {
+        secret: env('ADMIN_JWT_SECRET', ''),
+    },
   },
 });
